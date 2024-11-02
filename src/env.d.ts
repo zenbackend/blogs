3402bd0f1edb8765c926daf028cac27d9ef8c060
hema1909 <70359962+hema1909@users.noreply.github.com>
2024-11-02 09:40:54 +0000

    added log

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_GITHUB_TOKEN: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
